d10f41747afc0c6c101e65a434044193d4ec4a16
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-24 05:39:32 +0530

    Started UseEffect Learing

--- StudentPortal/src/pages/Home.jsx
+++ StudentPortal/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-[70vh]">
-      <h1 className="text-4xl font-bold">Welcome to Student Portal</h1>
+    <div className="flex justify-center items-center h-[70vh] ">
+      <h1 className="text-4xl font-bold ">Welcome to Student Portal</h1>
     </div>
   );
 };
