a1fc5da47be922432aec717d02d2a6dbff9f205c
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-24 06:12:04 +0530

    U[dated title using useEffect

--- UseEffect/src/components/UseEffect.jsx
+++ UseEffect/src/components/UseEffect.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
-  return (
-    <div>UseEffect</div>
-  )
-}
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = count;
+  }, [count]);
 
-export default UseEffect
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center h-screen gap-5">
+        <div>{count}</div>
+        <div className="space-x-4">
+          <button
+            className="btn btn-success p-2 rounded-lg font-bold"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increase
+          </button>
+          <button
+            className="btn btn-success p-2 rounded-lg font-bold"
+            onClick={() => setCount((prev) => prev - 1)}
+          >
+            Decrease
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
