4203faf74fbadf87132f98ecf5add30cd2d09e34
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-25 21:10:53 +0530

    React router creation done

--- react-router/RouteCreation/src/App.jsx
+++ react-router/RouteCreation/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  function Home() {
+    return <h1>Home Page</h1>;
+  }
 
-export default App
+  function About() {
+    return <h1>About Page</h1>;
+  }
+
+  function Contact() {
+    return <h1>Contact Page</h1>;
+  }
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
+
+export default App;
