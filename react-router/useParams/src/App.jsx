55299c9255cd2fbc629bfeae2b87513e3816c1df
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-28 00:39:13 +0530

    Size adjust

--- react-router/useParams/src/App.jsx
+++ react-router/useParams/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/users" element={<Users />} />
-          <Route path="/users/:id" element={<UserDetails />} />
+          <Route path="/users/:id" element={<UserDetails/>} />
         </Routes>
       </Router>
     </div>
