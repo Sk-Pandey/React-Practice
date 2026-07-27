897f78f987ab711e98f68be46d49238d5f9724c3
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-27 15:31:52 +0530

    NavBar Build

---
+++ StudentManagementSystem/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-rose-500">
+      <h1 className="text-2xl p-2 font-serif font-bold text-white">
+        Student Managment System
+      </h1>
+    </div>
+  );
+};
+
+export default Navbar;
