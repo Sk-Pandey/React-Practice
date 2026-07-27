e35a2f8f1138a2a722180f9f690a5af8b7b707a4
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-27 15:46:25 +0530

    Student Form Feature Added

--- StudentManagementSystem/src/components/StudentForm.jsx
+++ StudentManagementSystem/src/components/StudentForm.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const StudentForm = () => {
   return (
-    <div>StudentForm</div>
-  )
-}
+    <div>
+      <div className="flex  justify-center items-center flex-col mt-4 space-y-4">
+        <input
+          type="text"
+          id="name"
+          className="input"
+          placeholder="Enter Your Name"
+        />
+        <input
+          type="number"
+          id="age"
+          className="input"
+          placeholder="Enter Your Age"
+        />
+        <input
+          type="text"
+          id="course"
+          className="input"
+          placeholder="Enter Your Course"
+        />
+        <button className="btn  bg-success active:scale-95">Add Student</button>
+      </div>
+    </div>
+  );
+};
 
-export default StudentForm
+export default StudentForm;
