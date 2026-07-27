9479dfb1f5e81dc55515308f22523ebb11af4729
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-27 15:46:46 +0530

    Search bar added

--- StudentManagementSystem/src/components/SearchBar.jsx
+++ StudentManagementSystem/src/components/SearchBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className="space-x-4 flex justify-center items-center h-24">
+      <label className="input">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" required placeholder="Search Student" />
+      </label>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
