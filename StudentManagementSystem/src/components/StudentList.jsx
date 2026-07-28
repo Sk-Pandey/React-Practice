a91dbeba9328ffcfdd6575c9ab4b1938e577ebf6
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-28 15:56:58 +0530

    If no student found in search feature added

--- StudentManagementSystem/src/components/StudentList.jsx
+++ StudentManagementSystem/src/components/StudentList.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import StudentCard from "./StudentCard";
 
 const StudentList = ({ students, search, deleteStudent, editFn }) => {
+  const result = students.filter((student) => {
+    return student.name.toLowerCase().includes(search.toLowerCase());
+  });
   return (
     <div>
       <h1 className="text-center text-2xl font-bold">Students List</h1>
       <div className="flex justify-center items-center flex-wrap p-12 gap-8">
-        {students.length > 0 ? (
-          students
-            .filter((student) => {
-              return student.name.toLowerCase().includes(search.toLowerCase());
-            })
-            .map((student) => {
-              return (
-                <StudentCard
-                  student={student}
-                  key={student.id}
-                  deleteStudent={deleteStudent}
-                  editFn={editFn}
-                />
-              );
-            })
+        {result.length > 0 ? (
+          result.map((student) => {
+            return (
+              <StudentCard
+                student={student}
+                key={student.id}
+                deleteStudent={deleteStudent}
+                editFn={editFn}
+              />
+            );
+          })
         ) : (
           <h1 className="text-3xl font-extrabold font-serif text-gray-400">
             No Student Found
