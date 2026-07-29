d554f9f7cd1e2810df3b10f85cd04cf38d0f6541
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-29 21:05:49 +0530

    Creater router

---
+++ NextLearn/src/router/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Courses from "../pages/Courses";
+import Login from "../pages/Login";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+export default router;
