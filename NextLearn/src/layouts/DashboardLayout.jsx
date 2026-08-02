b2f312a907b1ca4ed4915b053263330f1fde395f
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-02 19:28:00 +0530

    Topbar

--- NextLearn/src/layouts/DashboardLayout.jsx
+++ NextLearn/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Topbar from "../components/Topbar";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
   return (
     <div>
-        
+      <Topbar />
+      <div className="flex justify-between">
+        <Sidebar />
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
