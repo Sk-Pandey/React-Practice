13b999c17bafc732751052d5af18afd2f541c37e
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-07 22:12:31 +0530

    SetSate Hide in Function

--- NextLearn/src/context/AuthProvider.jsx
+++ NextLearn/src/context/AuthProvider.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import { AuthContext } from "./AuthContext";
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const login = (userData) => {
+    setUser(userData);
+  };
+  const logout = () => {
+    setUser(null);
+  };
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
