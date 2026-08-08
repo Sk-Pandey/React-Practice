ce57d2a55181201f3a7589c86951ee2d5c5f8842
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-08 17:32:16 +0530

    Topbar updated

--- NextLearn/src/components/Topbar.jsx
+++ NextLearn/src/components/Topbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
-}
+  const { user } = useContext(AuthContext);
+  if (user) {
+    return (
+      <div>
+        <h1>{user.email}</h1>
+        <button className="btn btn-warning">logout</button>
+      </div>
+    );
+  }
+  return <div>Topbar</div>;
+};
 
-export default Topbar
+export default Topbar;
