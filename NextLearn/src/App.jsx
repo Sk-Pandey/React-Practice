f4a34e26ce840a77987e1944f5404f1206d76337
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-10 13:49:30 +0530

    App.jsx updated

--- NextLearn/src/App.jsx
+++ NextLearn/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Home />
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
