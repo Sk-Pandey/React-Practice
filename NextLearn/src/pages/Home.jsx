0ca09d06c2db0a24ff3b77057496974d3ffbe991
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-10 13:46:00 +0530

    Home Page Aded

---
+++ NextLearn/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main>
+      <h1>Welcome to NextLearn</h1>
+
+      <p>Learn development by building real projects.</p>
+    </main>
+  );
+};
+
+export default Home;
