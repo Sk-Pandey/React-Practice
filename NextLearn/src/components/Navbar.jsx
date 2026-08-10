c6b51f417e971351c477b4c6da56fe072643c59e
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-10 13:45:30 +0530

    NavBar added

---
+++ NextLearn/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>NextLearn</h1>
+
+      <div>
+        <span>Home</span>
+        <span>Courses</span>
+        <span>Login</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
