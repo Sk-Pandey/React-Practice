0e80453afdfab9426198444d633f335d30c15f27
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-11 22:07:45 +0530

    Router Provider Added

--- NextLearn/src/App.jsx
+++ NextLearn/src/App.jsx
@@ -1,13 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Navbar />
 
-      <Home />
+      <main>
+        <Outlet />
+      </main>
 
       <Footer />
     </div>
