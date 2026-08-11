ee45a7260770d33c15804a91e233dc8360bd668c
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-11 22:49:56 +0530

    Router Updated

--- NextLearn/src/router/router.jsx
+++ NextLearn/src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import CourseDetails from "../pages/CourseDetails";
+import Courses from "../pages/Courses";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "courses",
+        element: <Courses />,
+      },
+      {
+        path: "courses/:courseId",
+        element: <CourseDetails />,
+      },
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
