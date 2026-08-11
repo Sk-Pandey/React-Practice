d352d4d6f7c079db37acdb9c6a75e586062f5e11
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-11 22:49:37 +0530

    NavBar Updated

--- NextLearn/src/components/Navbar.jsx
+++ NextLearn/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav>
       <h1>NextLearn</h1>
 
       <div>
-        <span>Home</span>
-        <span>Courses</span>
-        <span>Login</span>
+        <NavLink to="/">Home</NavLink>
+
+        <NavLink to="/courses">Courses</NavLink>
+
+        <NavLink to="/login">Login</NavLink>
       </div>
     </nav>
   );
