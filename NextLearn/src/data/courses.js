8f07e7329b8ca521a9580b2865d0759e0da2209c
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-11 21:58:03 +0530

    Created Seperate Data file

---
+++ NextLearn/src/data/courses.js
@@ -0,0 +1,23 @@
+export const courses = [
+  {
+    id: 1,
+    title: "React",
+    description: "Learn React and build modern user interfaces.",
+    price: 3999,
+    duration: "6 Months",
+  },
+  {
+    id: 2,
+    title: "JavaScript",
+    description: "Master JavaScript from basics to advanced concepts.",
+    price: 2999,
+    duration: "5 Months",
+  },
+  {
+    id: 3,
+    title: "Node.js",
+    description: "Learn backend development using Node.js.",
+    price: 3499,
+    duration: "4 Months",
+  },
+];
