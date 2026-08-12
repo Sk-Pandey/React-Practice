d82394f28fdf60e314571cf7b462bcf37e447acf
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-12 21:25:55 +0530

    Log in Form Controlled Input added

--- NextLearn/src/pages/Login.jsx
+++ NextLearn/src/pages/Login.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
     <main>
       <h1>Login</h1>
 
-      <p>Login to continue to NextLearn.</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInput}
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInput}
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
     </main>
   );
 };
