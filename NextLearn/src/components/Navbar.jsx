4bed3126081028efa5c28db49103854d0d1afd77
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-08-14 21:38:49 +0530

    Navbar used contextAPI

--- NextLearn/src/components/Navbar.jsx
+++ NextLearn/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import { NavLink } from "react-router-dom";
-
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <nav>
       <h1>NextLearn</h1>
 
       <div>
         <NavLink to="/">Home</NavLink>
-
         <NavLink to="/courses">Courses</NavLink>
-
         <NavLink to="/login">Login</NavLink>
       </div>
+
+      {user && <p>Logged in as: {user.email}</p>}
     </nav>
   );
 };
-
 export default Navbar;
